3b4bb6dcc1d33a1e2627070cdd5e9fe28a49fc48
Will Deacon <will.deacon@arm.com>
2012-07-06 15:50:14 +0100

    BACKPORT: ARM: 7456/1: ptrace: provide separate functions for tracing syscall {entry,exit}
    The syscall_trace on ARM takes a `why' parameter to indicate whether or
    not we are entering or exiting a system call. This can be confusing for
    people looking at the code since (a) it conflicts with the why register
    alias in the entry assembly code and (b) it is not immediately clear
    what it represents.
    
    This patch splits up the syscall_trace function into separate wrappers
    for syscall entry and exit, allowing the low-level syscall handling
    code to branch to the appropriate function.
    
    Reported-by: Al Viro <viro@zeniv.linux.org.uk>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Change-Id: Id327b2feb3729175d1bb9caaf6288e7197d23460
    (backported from commit ad722541147e6e517a2077e3d944105e7bc4fa8e)
    [ kyle: backport to krillin: Also changed the ret_from_fork
      syscall_trace to a syscall_trace_exit. ]
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

--- kernel/arch/arm/kernel/ptrace.c
+++ kernel/arch/arm/kernel/ptrace.c
@@ -907,7 +907,13 @@ long arch_ptrace(struct task_struct *child, long request,
 	return ret;
 }
 
-asmlinkage int syscall_trace(int why, struct pt_regs *regs, int scno)
+enum ptrace_syscall_dir {
+	PTRACE_SYSCALL_ENTER = 0,
+	PTRACE_SYSCALL_EXIT,
+};
+
+static int ptrace_syscall_trace(struct pt_regs *regs, int scno,
+                                enum ptrace_syscall_dir dir)
 {
 	unsigned long ip;
 
@@ -927,9 +933,9 @@ asmlinkage int syscall_trace(int why, struct pt_regs *regs, int scno)
 	 * IP = 0 -> entry, =1 -> exit
 	 */
 	ip = regs->ARM_ip;
-	regs->ARM_ip = why;
+	regs->ARM_ip = dir;
 
-	if (why)
+	if (dir == PTRACE_SYSCALL_EXIT)
 		tracehook_report_syscall_exit(regs, 0);
 	else if (tracehook_report_syscall_entry(regs))
 		current_thread_info()->syscall = -1;
@@ -938,3 +944,18 @@ asmlinkage int syscall_trace(int why, struct pt_regs *regs, int scno)
 
 	return current_thread_info()->syscall;
 }
+
+asmlinkage int syscall_trace_enter(struct pt_regs *regs, int scno)
+{
+	int ret = ptrace_syscall_trace(regs, scno, PTRACE_SYSCALL_ENTER);
+	audit_syscall_entry(AUDIT_ARCH_ARM, scno, regs->ARM_r0, regs->ARM_r1,
+	                    regs->ARM_r2, regs->ARM_r3);
+	return ret;
+}
+
+asmlinkage int syscall_trace_exit(struct pt_regs *regs, int scno)
+{
+	int ret = ptrace_syscall_trace(regs, scno, PTRACE_SYSCALL_EXIT);
+	audit_syscall_exit(regs);
+	return ret;
+ }
