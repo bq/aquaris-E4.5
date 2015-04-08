34cf494bca625acad06274c3cba10aca148813c0
Pablo Moncada <pmoncadaisla@gmail.com>
2015-04-08 23:02:26 +0200

    Mislabelled headers that did not correspond for GPL release

--- mediatek/custom/common/kernel/hdmi/nxp_tda19989/tmNxCompId.h
+++ mediatek/custom/common/kernel/hdmi/nxp_tda19989/tmNxCompId.h
@@ -1,13 +1,5 @@
 /* -------------------------------------------------------------------------- */
 /* (C) Copyright 2000-2005              Koninklijke Philips Electronics N.V., */
-/*     All rights reserved                                                    */
-/*                                                                            */
-/* This source code and any compilation or derivative thereof is the          */
-/* proprietary information of Konlinklijke Philips Electronics N.V. and is    */
-/* Confidential in nature.                                                    */
-/* Under no circumstances is this software to be exposed to or placed under an*/
-/* Open Source License of any type without the expressed written permission of*/
-/* Koninklijke Philips Electronics N.V.                                       */
 /* -------------------------------------------------------------------------- */
 /*                                                                            */
 /* MoReUse - 2005-10-24   Version 118                                         */
